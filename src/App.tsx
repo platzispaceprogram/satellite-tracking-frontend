3d706ea5ba63ecaa59ca07d8a56e4639d5815755
Angel Eduardo Gaxiola Javier <angel.gaxiola@sonorasoft.com>
2023-07-22 17:23:34 -0700

    Feature: Add home

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>SPACE PROGRAM</h1>
+      < Home />
     </>
   )
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import videoBg from '../assets/videoBg.mp4'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className='overlay'/>
+            <video src={videoBg} autoPlay loop muted/>
+            <div className='content'>
+            <h1>NAME TEAM</h1>
+            <p>PLATZI SPACE PROGRAM</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
