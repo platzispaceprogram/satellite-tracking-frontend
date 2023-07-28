3c036c5d137b5a2c7e7212a9dc5a698660a17797
angeleduardogj <angeleduardogj@gmail.com>
2023-07-28 11:19:33 -0700

    Fix typescript

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -9,7 +9,7 @@ function BasicFormControl() {
     <FormControl defaultValue="" required>
       <Label>Name</Label>
       <StyledInput placeholder="Write your name here" />
-      <HelperText />
+      {/* <HelperText /> */}
     </FormControl>
   );
 }
@@ -76,28 +76,28 @@ const Label = styled(
   }
 `;
 
-const HelperText = styled((props: '{}') => {
-  const formControlContext = useFormControlContext();
-  const [dirty, setDirty] = React.useState(false);
+// const HelperText = styled((props: '{}') => {
+//   const formControlContext = useFormControlContext();
+//   const [dirty, setDirty] = React.useState(false);
 
-  React.useEffect(() => {
-    if (formControlContext?.filled) {
-      setDirty(true);
-    }
-  }, [formControlContext]);
+//   React.useEffect(() => {
+//     if (formControlContext?.filled) {
+//       setDirty(true);
+//     }
+//   }, [formControlContext]);
 
-  if (formControlContext === undefined) {
-    return null;
-  }
+//   if (formControlContext === undefined) {
+//     return null;
+//   }
 
-  const { required, filled } = formControlContext;
-  const showRequiredError = dirty && required && !filled;
+//   const { required, filled } = formControlContext;
+//   const showRequiredError = dirty && required && !filled;
 
-  return showRequiredError ? <p {...props}>This field is required.</p> : null;
-})`
-  font-family: 'IBM Plex Sans', sans-serif;
-  font-size: 0.875rem;
-`;
+//   return showRequiredError ? <p {...props}>This field is required.</p> : null;
+// })`
+//   font-family: 'IBM Plex Sans', sans-serif;
+//   font-size: 0.875rem;
+// `;
 
 const blue = {
   100: '#DAECFF',
--- src/pages/2.project/Project.tsx
+++ src/pages/2.project/Project.tsx
@@ -1,64 +1,64 @@
-import * as React from 'react';
-import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
+// import * as React from "react";
+// import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
 
-import Box from '@mui/joy/Box';
-import CssBaseline from '@mui/joy/CssBaseline';
-import IconButton from '@mui/joy/IconButton';
+// import Box from "@mui/joy/Box";
+// import CssBaseline from "@mui/joy/CssBaseline";
+// import IconButton from "@mui/joy/IconButton";
 
+// function ColorSchemeToggle() {
+//   const { mode, setMode } = useColorScheme();
+//   const [mounted, setMounted] = React.useState(false);
+//   React.useEffect(() => {
+//     setMounted(true);
+//   }, []);
+//   if (!mounted) {
+//     return null;
+//   }
+//   return (
+//     <IconButton
+//       id="toggle-mode"
+//       size="lg"
+//       variant="soft"
+//       color="neutral"
+//       onClick={() => {
+//         if (mode === 'light') {
+//           setMode('dark');
+//         } else {
+//           setMode('light');
+//         }
+//       }}
+//       sx={{
+//         position: 'fixed',
+//         zIndex: 999,
+//         top: '1rem',
+//         right: '1rem',
+//         borderRadius: '50%',
+//         boxShadow: 'sm',
+//       }}
+//     >
 
-function ColorSchemeToggle() {
-  const { mode, setMode } = useColorScheme();
-  const [mounted, setMounted] = React.useState(false);
-  React.useEffect(() => {
-    setMounted(true);
-  }, []);
-  if (!mounted) {
-    return null;
-  }
-  return (
-    <IconButton
-      id="toggle-mode"
-      size="lg"
-      variant="soft"
-      color="neutral"
-      onClick={() => {
-        if (mode === 'light') {
-          setMode('dark');
-        } else {
-          setMode('light');
-        }
-      }}
-      sx={{
-        position: 'fixed',
-        zIndex: 999,
-        top: '1rem',
-        right: '1rem',
-        borderRadius: '50%',
-        boxShadow: 'sm',
-      }}
-    >
-      
-    </IconButton>
-  );
-}
+//     </IconButton>
+//   );
+// }
 
 export default function TeamExample() {
   return (
-    <CssVarsProvider disableTransitionOnChange>
-      <CssBaseline />
-      <ColorSchemeToggle />
-      <Box
-        sx={{
-          height: '100vh',
-          overflowY: 'scroll',
-          scrollSnapType: 'y mandatory',
-          '& > div': {
-            scrollSnapAlign: 'start',
-          },
-        }}
-      >
-       
-      </Box>
-    </CssVarsProvider>
+    // <CssVarsProvider disableTransitionOnChange>
+    //   <CssBaseline />
+    //   <ColorSchemeToggle />
+    //   <Box
+    //     sx={{
+    //       height: '100vh',
+    //       overflowY: 'scroll',
+    //       scrollSnapType: 'y mandatory',
+    //       '& > div': {
+    //         scrollSnapAlign: 'start',
+    //       },
+    //     }}
+    //   >
+
+    //   </Box>
+    // </CssVarsProvider>
+    <div>Project</div>
   );
 }
